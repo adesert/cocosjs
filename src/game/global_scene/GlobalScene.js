b7e68039472db8e24a341dd5852cd1205d35a910
DD <dj2@macmini-dj2.local>
2015-12-03 09:42:26 +0800

    ant

---
+++ src/game/global_scene/GlobalScene.js
@@ -0,0 +1,5 @@
+
+var GlobalScene = function  () {
+	
+}
+
--- src/module_consts.js
+++ src/module_consts.js
@@ -1,14 +1,14 @@
 
 var WND_NAME = {
-    this.SOCRE_AWARDS = "SOCRE_AWARDS",                  -- 分关分数结算界面
-    this.LEVEL_ENTER_WNDS = "LEVEL_ENTER_WNDS",          -- 进入战斗前关卡进入界面
-    this.SHOP_WNDS = "SHOP_WNDS",                        -- 商城
-    this.TILI_BUYS = "TILI_BUYS",                        -- 体力快速购买
-    this.LEVEL_MAP = "LEVEL_MAP",                        -- 关卡地图
-    this.LOGIN_WNDS = "LOGIN_WNDS",                      -- 登陆界面
-    this.FAILURE_WND = "FAILURE_WND",                    -- 通关失败
-    this.WIN_WND = "WIN_WNDS",                           -- 成功
-    this.FIGHTING_IN =  "FIGHTING_IN",                   -- 战斗中，let's go ,let's go
+    this.SOCRE_AWARDS = "SOCRE_AWARDS",                  //-- 分关分数结算界面
+    this.LEVEL_ENTER_WNDS = "LEVEL_ENTER_WNDS",          //-- 进入战斗前关卡进入界面
+    this.SHOP_WNDS = "SHOP_WNDS",                        //-- 商城
+    this.TILI_BUYS = "TILI_BUYS",                        //-- 体力快速购买
+    this.LEVEL_MAP = "LEVEL_MAP",                        //-- 关卡地图
+    this.LOGIN_WNDS = "LOGIN_WNDS",                      //-- 登陆界面
+    this.FAILURE_WND = "FAILURE_WND",                    //-- 通关失败
+    this.WIN_WND = "WIN_WNDS",                           //-- 成功
+    this.FIGHTING_IN =  "FIGHTING_IN",                   //-- 战斗中，let's go ,let's go
     this.test_ui = "test_ui",
 }
 
